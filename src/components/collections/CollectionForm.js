166fb19dc65b57ea692d5e74cd270de86a34d5ed
jmason87 <joewebdev87@gmail.com>
2022-03-10 15:21:01 -0600

    collection form renders and works properly

--- src/components/collections/CollectionForm.js
+++ src/components/collections/CollectionForm.js
@@ -1,10 +1,63 @@
 import React, { useEffect, useState } from "react"
-import { getCollections } from "./CollectionManager"
+import { createCollection, getCollections } from "./CollectionManager"
 import { useHistory } from "react-router-dom"
+import { getCurrentUser } from "../users/UserManager"
 
 
 export const CollectionForm = () => {
+    const [currentUser, setCurrentUser] = useState({})
+    const date = new Date()
+    const history = useHistory()
+    const [collection, setCollection] = useState({
+        name: "",
+        date: "",
+        user: 0
+    })
+
+    useEffect(() => {
+        getCurrentUser().then(setCurrentUser)
+    }, [])
+    console.log(currentUser.id)
+    const submitNewCollection = (evt) => {
+        evt.preventDefault()
+        const date = new Date()
+        const newCollection = {
+            name: collection.name,
+            date: date.toISOString().split('T')[0],
+            user: parseInt(currentUser.id)
+        }
+        createCollection(newCollection).then(() => {history.push('/collections')})
+    }
+
+    // const submitNewCollection = (evt) => {
+    //     evt.preventDefault()
+    //     createCollection(collection)
+    //         .then(() => {
+    //             history.push('/collections')
+    //         })
+    // }
+
+
     return <>
         <h1>New Collection Form</h1>
+        <form>
+            <section>
+                <label>Name:</label>
+                <input
+                    type="text"
+                    onChange={
+                        (evt) => {
+                            const copy = {...collection}
+                            copy.name = evt.target.value
+                            setCollection(copy)
+                        }
+                    }>
+                </input>
+                <div>
+                    <button onClick={submitNewCollection}>Submit</button>
+                </div>
+            </section>
+        </form>
+
     </>
 }
--- src/components/collections/CollectionManager.js
+++ src/components/collections/CollectionManager.js
@@ -6,3 +6,18 @@ export const getCollections = () => {
     })
     .then(res => res.json())
 }
+
+export const createCollection = (newCollection) => {
+    
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`,
+            "content-Type": "application/json"
+        },
+        body: JSON.stringify(newCollection)
+    }
+    return fetch(`http://localhost:8000/collections`, fetchOptions)
+        .then(getCollections)
+
+}
