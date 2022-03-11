35a86e5096205258cb16e2070d4f66577fdde9cb
jmason87 <joewebdev87@gmail.com>
2022-03-11 10:50:04 -0600

    added delete button functionality for collections

--- src/components/collections/CollectionDetail.js
+++ src/components/collections/CollectionDetail.js
@@ -8,27 +8,27 @@ export const CollectionDetail = () => {
     const [collection, setCollection] = useState({})
     const { collectionId } = useParams()
     const parsedId = parseInt(collectionId)
+    
     useEffect(() => {
         getSingleCollection(parsedId).then(setCollection)
     }, [])
     
-    console.log(parsedId)
     return <>
         <h1>Collection Detail</h1>
         <div>
             <button>Add Cards</button>
         </div>
         <div>
-            {
+            {   // mapping over the card array in the collection object
                 collection.card?.map((card) => {
                     return <>
-                    <img key={`card--${card.id}`}src={card.image} />
+                    <img key={`card--${card.id}`} src={card.image} />
                     <p>{card.first_name} {card.last_name}</p>
                     <p>Card #{card.card_number}</p>
                     <p>Category: {card.card_category.label}</p>
                     <p>Set: {card.set?.name}</p>
                     <ul>Attributes: 
-                    {
+                    { // mapping over the tags array in each card object
                         card.tag.map((cardtags) => {
                             return <li key={`cardtags--${cardtags.id}`}>{cardtags.label}</li>
                         })
