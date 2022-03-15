0001a6670c151384e0c3a1bf5c0b40691a3cf2ed
jmason87 <joewebdev87@gmail.com>
2022-03-15 14:50:26 -0500

    added ability to delete cards from collections

--- src/components/collections/CollectionDetail.js
+++ src/components/collections/CollectionDetail.js
@@ -1,19 +1,38 @@
 import React, { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { useHistory } from "react-router-dom"
+import { deleteCardCollection, getCardCollections } from "../cardcollection/CardCollectionManager"
 import { getSingleCollection } from "./CollectionManager"
 
 
 export const CollectionDetail = () => {
     const [collection, setCollection] = useState({})
+    const [cardsInCollection, setCardsInCollection] = useState([])
     const { collectionId } = useParams()
     const parsedId = parseInt(collectionId)
     const history = useHistory()
     
+    // this useEffect is watch for state change in the cardsInCollection array, every time that state changes, this useEffect will run
     useEffect(() => {
         getSingleCollection(parsedId).then(setCollection)
+    }, [cardsInCollection])
+
+    useEffect(() => {
+        getCardCollections().then(setCardsInCollection)
     }, [])
-    
+
+    // this function is looking to see if the card and collection in any cardcollection object exactly matches the card and collection
+    // that we are looking at in the dom. We get the cardId parameter from the collection.card map below. If there is a match, the 
+    // card collection object is deleted from the database and the cardsInCollection state is updated.
+    const foundCardToRemove = (cardId) => {
+        cardsInCollection.map((c) => {
+            if (c.card === cardId && c.collection === collection.id) {
+                return deleteCardCollection(c.id).then(setCardsInCollection)
+            }
+        })
+
+    }
+
     return <>
         <h1>Collection Detail</h1>
         <div>
@@ -35,7 +54,10 @@ export const CollectionDetail = () => {
                     }
                     </ul>
                     <div>
-                    <button>Remove Card</button>
+                    <button 
+                    value={card.id}
+                    //we invoke the function onClick and pass in the card.id as the argument for cardId parameter we set above  
+                    onClick={() => {foundCardToRemove(card.id)}}>Remove Card</button>
                     </div>
                     </>
                 })
